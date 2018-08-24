240567623b9f64cc58beb6aae4407b13ae19066d
Conner Chyung <chyungspice@gmail.com>
2018-08-24 14:55:45 -0700

    allow all hosts

--- tshirtpricetool/settings/base.py
+++ tshirtpricetool/settings/base.py
@@ -28,7 +28,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*'
+]
 
 
 # Application definition
