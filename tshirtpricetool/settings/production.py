621f7c17be923e8896613e0e58b772874dc2698f
Conner Chyung <chyungspice@gmail.com>
2018-08-24 15:40:49 -0700

    remove django heroku

--- tshirtpricetool/settings/production.py
+++ tshirtpricetool/settings/production.py
@@ -1,11 +1,8 @@
 from tshirtpricetool.settings.base import *
 import dj_database_url
-import django_heroku
 
 DEBUG = True
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-django_heroku.settings(locals())
