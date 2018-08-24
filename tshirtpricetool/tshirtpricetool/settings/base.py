bda434a472bb4dd863f93ae8fe6784c11adae795
Conner Chyung <chyungspice@gmail.com>
2018-08-24 14:38:13 -0700

    fix typo in base settings

--- tshirtpricetool/tshirtpricetool/settings/base.py
+++ tshirtpricetool/tshirtpricetool/settings/base.py
@@ -127,6 +127,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIS_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
