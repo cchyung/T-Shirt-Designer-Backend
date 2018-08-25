70115386faac5ef3e831207bfba3a812c0a94a87
Conner Chyung <chyungspice@gmail.com>
2018-08-25 16:54:03 -0700

    trying new configuration for database

--- tshirtpricetool/settings/production.py
+++ tshirtpricetool/settings/production.py
@@ -4,4 +4,10 @@ import dj_database_url
 DEBUG = True
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': db_from_env
+}
