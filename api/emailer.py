d548564aa7944d98ce4a739cbd630601ef0ac4e9
Conner Chyung <chyungspice@gmail.com>
2018-10-24 21:11:40 -0700

    fix unused imports

--- api/emailer.py
+++ api/emailer.py
@@ -1,12 +1,8 @@
-from django.core.mail import EmailMultiAlternatives, send_mail
-import os
-from email.mime.image import MIMEImage
+from django.core.mail import EmailMultiAlternatives
 
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from tshirtpricetool.settings.base import get_env_variable
-import re
-import cStringIO
 from api import models
 
 
@@ -49,7 +45,3 @@ def send_report(style_id, quantities, ink_colors, addon_ids, email, comments, pr
 
     msg.send()
 
-
-def decode_image(image_string):
-    image_data = re.sub('^data:image/.+;base64,', '', image_string).decode('base64')
-    return MIMEImage(cStringIO.StringIO(image_data))
