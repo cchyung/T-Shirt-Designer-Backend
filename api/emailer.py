24d6c8f59a84c773eec7116962962c4c0e9fe0e5
cchyung <chyungspice@gmail.com>
2020-04-11 14:21:37 -0600

    update emailer and image template

--- api/emailer.py
+++ api/emailer.py
@@ -1,10 +1,19 @@
 from django.core.mail import EmailMultiAlternatives
+from email.mime.image import MIMEImage
 
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from tshirtpricetool.settings.base import get_env_variable
 from api import models
+from base64 import decodebytes
 
+
+def decode_image(image_data, image_name):
+    image_data = bytes(image_data[image_data.find(",")+1:], encoding='utf8')
+    img = MIMEImage(decodebytes(image_data), 'jpeg')
+    img.add_header('Content-ID', f'<%s.jpeg>' % image_name)
+    img.add_header("Content-Disposition", "inline", filename=f'%s.jpeg' % image_name)
+    return img
 
 def send_report(style_id, quantities, ink_colors, addon_ids, email, name, comments, price, front_image, back_image):
     from_email = "Kastlfel <" + get_env_variable('EMAIL_REPORT_FROM') + ">"
@@ -36,10 +45,12 @@ def send_report(style_id, quantities, ink_colors, addon_ids, email, name, commen
 
     msg = EmailMultiAlternatives(email_subject, text_content, from_email, to_emails)
 
-    # embed images
-    # front_email_image = decode_image(front_image)
+    front_img = decode_image(front_image, 'front-image')
+    back_img = decode_image(back_image, 'back-image')
+    msg.attach(front_img)
+    msg.attach(back_img)    
+
     # front_email_image.add_header('Content-ID', '<front>')
-    # back_email_image = decode_image(back_image)
     # back_email_image.add_header('Content-ID', '<back>')
     # msg.attach(front_email_image)
     # msg.attach(back_email_image)
--- tshirtpricetool/settings/local.py
+++ tshirtpricetool/settings/local.py
@@ -5,13 +5,24 @@ DEBUG = True
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'NAME': 'tshirtdesigner',
+#         'USER': 'admin',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'NAME': 'tshirtdesigner',
-        'USER': 'admin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': 'd23amtt7hdlrgt',
+        'USER': 'pwezlcfidhdgui',
+        'PASSWORD': '68268732ca70c8ac35c0e76bf57f2e05c928ee6baa02114e6fb867cf7d42eab3',
+        'HOST': 'ec2-54-243-216-33.compute-1.amazonaws.com',
+        'PORT': '5432',
         'ENGINE': 'django.db.backends.postgresql_psycopg2'
     }
 }
